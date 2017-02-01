009718f634d3b29d0ace17bc1b1071c6bb17c525
Dustin Downing <dustin.downing@layrconsulting.com>
2017-01-31 19:24:33 -0600

    main-tile

--- angular.js
+++ angular.js
@@ -1,20 +1,15 @@
-angular.module('myApp', [])
-	.controller('MockController', ['$scope', function($scope) {
-		$scope.notifications = [
-			{
-				title: "Project Created"
-			},
-			{
-				title: "Labor Forcast Generated"
-			}
-		];
-	}])
-	.directive('notification', function() {
-	  return {
-	  	restrict: 'E',
-	  	scope: {
-	  		info: '=',
-	  	},
-	    templateUrl: "notification.html"
-	  };
-	});
+angular.module('HomePage', ['ngMaterial'])
+
+.controller('AppCtrl', function($scope) {
+  $scope.imagePath = 'img/washedout.png';
+  $scope.header = "Hi, i'm Dustin Downing";
+  $scope.subHeader = "Full Stack Web Developer";
+  $scope.discription = "Freelance software developer in the Austin, TX area."
+
+})
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+  $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+  $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+  $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+});
