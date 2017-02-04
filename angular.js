6a5858136f1d812bd7c04318a4f61e1f7b36c9de
Dustin Downing <dustin.downing@layrconsulting.com>
2017-02-04 17:41:34 -0600

    skills page finished

--- angular.js
+++ angular.js
@@ -4,10 +4,13 @@ angular.module('HomePage', ['ngMaterial'])
   $scope.imagePath = 'img/washedout.png';
   $scope.header = "Hi, i'm Dustin Downing!";
   $scope.subHeader = "Full Stack Web Developer";
-  $scope.discription = "Freelance software engineer in the Austin, TX area specializing in web application business solutions."
+  $scope.discription = "Freelance software engineer in the Austin, TX area specializing in web applications for custom business solutions."
 
 })
 .config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('indigo')
+    .accentPalette('orange');
   $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
   $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
   $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
