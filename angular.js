d0a0f48ea72e7079f29a880da85541afa6213995
Dustin Downing <dustin.downing@layrconsulting.com>
2017-09-08 13:42:49 -0500

    updating frameworks

--- angular.js
+++ angular.js
@@ -4,7 +4,7 @@ angular.module('HomePage', ['ngMaterial'])
   $scope.imagePath = 'img/washedout.png';
   $scope.header = "Hi, i'm Dustin Downing!";
   $scope.subHeader = "Full Stack Web Developer";
-  $scope.discription = "Freelance software engineer in the Austin, TX area specializing in web applications for custom business solutions."
+  $scope.discription = "Freelance software developer in the Austin, TX area specializing in web applications for custom business solutions."
 
 })
 .config(function($mdThemingProvider) {
